0adff41ec06dcc95c64f4899c621baea2d63fb23
GnaneswariChanda <gnaneswarichanda@gmail.com>
2025-06-04 14:40:42 +0530

    Add files via upload

---
+++ Main17.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+public class Main17
+{
+	public static void main(String[] args) {
+	Stack s=new Stack<>();
+	s.push(1);
+	s.push(2);
+    s.push(3);
+	System.out.println(s.peek());
+	System.out.println(s.pop());
+	}
+}
